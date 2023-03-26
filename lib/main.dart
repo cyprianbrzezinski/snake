77edec016ab8ee45af827142affd3804b98670af
Cyprian Brzezinski <cyprian200454@gmail.com>
2023-03-26 16:42:56 +0200

    snake

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
-      ),
-      home: const Snake(),
+    return const MaterialApp(
+    
+ 
+      home: Snake(),
     );
   }
 }
